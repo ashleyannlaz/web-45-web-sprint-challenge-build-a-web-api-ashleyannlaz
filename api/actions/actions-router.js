532593f190f79b1ddbd6085615c734e3f53f5772
ashleyannlaz <paperpastels@gmail.com>
2021-09-03 13:49:32 -0400

    Get Actions Complete

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,21 @@
-// Write your "actions" router here!
+const express = require('express');
+const router = express();
+const Actions = require('./actions-model')
+
+router.get('/', (req, res, next) => {
+    Actions.get()
+    .then(actions => {
+        res.status(200).json(actions)
+    })
+    .catch(next)
+})
+
+router.use((err, req, res, next) => {
+    console.log(err.message);
+    res.status(err.status || 500).json({
+      message: err.message,
+      customMessage: "Something in users router!",
+    });
+  });
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -3,11 +3,14 @@ const server = express();
 const cors = require("cors");
 
 const projectsRouter = require('./projects/projects-router')
+const actionsRouter = require('./actions/actions-router')
 
 server.use(express.json())
 server.use(cors());
 
 server.use('/api/projects', projectsRouter )
+server.use('/api/actions', actionsRouter )
+
 
 server.get('/', (req, res) => {
     res.send(`<h2>Let's write some middleware!</h2>`);
